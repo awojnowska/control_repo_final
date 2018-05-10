fe7d546e6d14ddae3207384f87866701d52cdee3
Anthony Leto <anthony@fms.io>
2018-05-10 14:40:49 +0000

    Windows-fied admins.pp

--- modules/system_users/manifests/admins.pp
+++ modules/system_users/manifests/admins.pp
@@ -1,12 +1,21 @@
 class system_users::admins {
-
   group { 'staff':
     ensure => present,
   }
 
-  user { 'admin':
-    group => 'staff',
-    shell => '/bin/csh',
-  }
+  if $facts['kernel'] == 'windows' {
+    user { 'admin':
+      group => 'staff'
+    }
+  } else {
+    package { 'csh':
+      ensure => latest,
+    }
 
+    user { 'admin':
+      group   => 'staff',
+      shell   => '/bin/csh',
+      require => Package['csh'],
+    }
+  }
 }
