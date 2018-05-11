0092f8143be5a1877f0f4ae735d0e469e9ee76d3
Anthony Leto <anthony@fms.io>
2018-05-11 14:11:55 +0000

    Ready to test config

--- modules/nginxformasses/manifests/init.pp
+++ modules/nginxformasses/manifests/init.pp
@@ -1,10 +1,25 @@
 class nginxformasses (
-  String $docroot = "/var/www",
+  String $docroot    = "/var/www",
+  String $portnumber = "80",
 ) {
 
   if $facts['kernel'] =~ /[Ll]inux/ {
     package { 'nginx':
       ensure => latest,
+    }
+
+    file { '/etc/nginx/sites-available/default':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      content => template('${module_name}/default.erb'),
+      require => Package['nginx'],
+      notify  => Service['nginx'],
+    }
+
+    service { 'nginx':
+      ensure  => running,
+      require => Package['nginx'],
     }
   } else {
     notify { 'Negative Nancy': }
