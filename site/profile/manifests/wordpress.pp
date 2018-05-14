3d602472f67f8043e2207f435616b0a80f410264
Ashley <ashley.white@infrotex.co.uk>
2018-05-14 14:49:38 +0000

    add Mysql

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -10,4 +10,21 @@ service { 'apache2':
   enable   => 'true',
   require  => Package['apache2'],
 }
+
+package { 'mysql':
+  ensure => present,
+}
+
+service {'mysql':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['mysql'],
+}
+
+# package { 'wordpress':
+#   wp_owner    => 'wordpress',
+#   wp_group    => 'wordpress',
+#   db_user     => 'wordpress',
+#   db_password => 'hvyH(S%t(\"0\"16',
+# }
 }
