1bde5b29c8558da6e0f0ffeec137ba09c45da56b
Anthony Leto <anthony@fms.io>
2018-05-14 10:13:29 +0100

    Added IIS Server and Configuration

--- modules/nginxformasses/manifests/init.pp
+++ modules/nginxformasses/manifests/init.pp
@@ -1,6 +1,6 @@
 class nginxformasses (
-  String $docroot    = "/var/www",
-  String $portnumber = "80",
+  String $docroot    = '/var/www',
+  String $portnumber = '80',
 ) {
 
   if $facts['kernel'] =~ /[Ll]inux/ {
@@ -21,7 +21,7 @@ class nginxformasses (
       ensure  => running,
       require => Package['nginx'],
     }
-    
+
     file { $docroot:
       owner   => 'www-data',
       group   => 'www-data',
@@ -29,13 +29,13 @@ class nginxformasses (
       recurse => true,
       require => Package['nginx'],
     }
-  
+
     file { "${docroot}/index.html":
       owner   => 'www-data',
       group   => 'www-data',
       mode    => '0755',
       source  => "puppet:///modules/${module_name}/index.html",
-      require => File["${docroot}"],
+      require => File[$docroot],
     }
   } else {
     notify { 'Negative Nancy': }
@@ -45,9 +45,39 @@ class nginxformasses (
       provider => 'chocolatey',
       notify   => Reboot['Reboot-PowerShell'],
     }
-    
+
     reboot { 'Reboot-PowerShell':
       apply => finished,
-    }      
-  } 
+    }
+
+    dsc_windowsfeature {'IIS':
+      dsc_ensure => 'present',
+      dsc_name   => 'Web-Server',
+      notify     => Reboot['LightDavesHair'],
+    }
+
+    dsc_windowsfeature {'IIS-Scripting-Tools':
+      dsc_ensure => 'present',
+      dsc_name   => 'Web-Scripting-Tools',
+      notify     => Reboot['LightDavesHair'],
+    }
+
+    reboot { 'LightDavesHair':
+      apply => finished,
+    }
+
+    iis_site { 'minimal':
+      ensure          => 'started',
+      physicalpath    => 'c:\\inetpub\\minimal',
+      applicationpool => 'DefaultAppPool',
+      require         => [File['IIS Minimalists Directory'],
+                          Dsc_windowsfeature['IIS-Scripting-Tools'],
+                          Dsc_windowsfeature['IIS']],
+    }
+
+    file { 'IIS Minimalists Directory':
+      ensure => directory,
+      path   => 'c:\\inetpub\\minimal',
+    }
+  }
 }
